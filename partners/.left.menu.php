fff5a4c27019019b1ee28e28acf88325028e6fa5
ftp <santiag0_tech@pizza.beget.ru>
2024-10-18 13:36:07 +0300

    first commit

---
+++ partners/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"/partners/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнером", 
+		"/partners/how_to_become.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/partners/events_schedule/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ partners/events_schedule/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Анонсы", 
+		"/partners/events_schedule/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Архив", 
+		"/partners/events_schedule/arсhive.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
